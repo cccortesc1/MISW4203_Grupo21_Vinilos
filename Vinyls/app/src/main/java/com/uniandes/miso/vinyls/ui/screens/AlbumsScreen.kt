449c57892ffdefd097b39208aa2be697d997e5bc
Felipe Leyva <d.leyvad@uniandes.edu.co>
2023-04-30 14:39:56 -0500

    actualizacion de la estructura [HU01]

--- Vinyls/app/src/main/java/com/uniandes/miso/vinyls/ui/screens/AlbumsScreen.kt
+++ Vinyls/app/src/main/java/com/uniandes/miso/vinyls/ui/screens/AlbumsScreen.kt
@@ -3,11 +3,13 @@ package com.uniandes.miso.vinyls.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,10 +19,15 @@ import com.uniandes.miso.vinyls.viewmodels.AlbumViewModel
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import com.uniandes.miso.vinyls.R
+import com.uniandes.miso.vinyls.models.Album
+import com.uniandes.miso.vinyls.models.Collector
+import com.uniandes.miso.vinyls.utils.MainAppBar
 
 
 @Composable
@@ -28,54 +35,71 @@ fun AlbumsScreen(
     albumViewModel: AlbumViewModel = hiltViewModel(),
     navController: NavHostController) {
 
-    val albums by albumViewModel.albums.observeAsState(emptyList())
+    val albumItems = albumViewModel.albums.observeAsState()
+    Scaffold(
+        topBar = { MainAppBar(navController, R.string.albums) }
+    ) { padding ->
+        AlbumMediaList(
+            modifier = Modifier.padding(padding),
+            albumItems
+        )
+    }
+}
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
+@Composable
+fun AlbumMediaList(modifier: Modifier, albumItems: State<List<Album>?>) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(dimensionResource(R.dimen.cell_min_width)),
+        contentPadding = PaddingValues(dimensionResource(R.dimen.padding_xsmall)),
+        modifier = modifier
     ) {
-
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Text(
-                text = "Álbumes",
-                style = MaterialTheme.typography.h4,
-                color = Color.Black
-            )
-
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
-            ) {
-
-                items(albums) { album ->
-                    // carga la imagen desde la url en un painter
-                    val painter = rememberAsyncImagePainter(
-                        ImageRequest.Builder(LocalContext.current).data(data = album.cover)
-                            .apply(block = fun ImageRequest.Builder.() {
-                                size(200, 200)
-                            }).build()
-                    )
-
-                    // Muestra la imagen con el painter
-                    Image(
-                        painter = painter,
-                        contentDescription = "Imagen del album ${album.name}",
-                        modifier = Modifier.size(200.dp),
-                        contentScale = ContentScale.Crop
-                    )
-
-                    Text(
-                        text = album.name,
-                        style = MaterialTheme.typography.h6,
-                        color = Color.Black
-                    )
-                }
+        albumItems.value?.let { listAlbum ->
+            items(listAlbum) {
+                AlbumMediaListItem(
+                    albumItem = it
+                )
             }
         }
+    }
+}
+
+@Composable
+fun AlbumMediaListItem(
+    albumItem: Album
+) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .height(150.dp),
+        elevation = 10.dp
+    ) {
+        AlbumItem(albumItem)
+    }
+}
+
+@Composable
+fun AlbumItem(albumItem: Album) {
+
+    Column(
+        modifier = Modifier
+            .padding(15.dp)
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        val painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(LocalContext.current).data(data = albumItem.cover)
+                .apply(block = fun ImageRequest.Builder.() {
+                    size(200, 200)
+                }).build()
+        )
+
+        // Muestra la imagen con el painter
+        Image(
+            painter = painter,
+            contentDescription = "Imagen del album ${albumItem.name}",
+            modifier = Modifier.size(200.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
