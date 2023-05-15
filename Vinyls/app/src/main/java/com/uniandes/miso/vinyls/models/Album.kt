119508645f8f2fdc4e415a30f5864dc203085cec
Felipe Leyva <d.leyvad@uniandes.edu.co>
2023-05-14 22:32:45 -0500

    HU02: Se agrega funcionalidad de detalle del album

--- Vinyls/app/src/main/java/com/uniandes/miso/vinyls/models/Album.kt
+++ Vinyls/app/src/main/java/com/uniandes/miso/vinyls/models/Album.kt
@@ -1,5 +1,8 @@
 package com.uniandes.miso.vinyls.models
 
+import android.net.Uri
+import com.google.gson.Gson
+
 data class Album (
     val albumId: Int,
     val name: String,
@@ -7,5 +10,24 @@ data class Album (
     val releaseDate: String,
     val description: String,
     val genre: String,
-    val recordLabel: String
+    val recordLabel: String,
+    var tracks: List<Track>,
+    var performers: List<Performer>,
+    var comments: List<Comment>
+) {
+    override fun toString(): String = Uri.encode(Gson().toJson(this))
+}
+
+data class Performer (
+    val id: Long,
+    val name: String,
+    val image: String,
+    val description: String,
+    val birthDate: String
+)
+
+data class Track (
+    val trackId: Int,
+    val name: String,
+    val duration: String
 )
--- Vinyls/app/src/main/java/com/uniandes/miso/vinyls/utils/Utils.kt
+++ Vinyls/app/src/main/java/com/uniandes/miso/vinyls/utils/Utils.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import com.google.gson.Gson
+import com.uniandes.miso.vinyls.models.Album
 import com.uniandes.miso.vinyls.models.Collector
 
 enum class User(val idUser: String) {
@@ -32,6 +33,14 @@ fun MainAppBar(navController: NavHostController, titleResource: Int) {
         })
 }
 
+
+class AlbumArgType : JsonNavType<Album>() {
+    override fun fromJsonParse(value: String): Album =
+        Gson().fromJson(value, Album::class.java)
+
+    override fun Album.getJsonParse(): String = Gson().toJson(this)
+
+}
 
 class CollectorArgType : JsonNavType<Collector>() {
     override fun fromJsonParse(value: String): Collector =
