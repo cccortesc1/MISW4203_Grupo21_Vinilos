fe20ca4d5630ffb88a914d013c1f38ffa55831de
cccortesc1 <cc.cortesc1@uniandes.edu.co>
2023-05-24 21:21:44 -0500

    Se realizan ajustes en la historia de crear album

--- Vinyls/app/src/main/java/com/uniandes/miso/vinyls/ui/screens/AlbumCreateScreen.kt
+++ Vinyls/app/src/main/java/com/uniandes/miso/vinyls/ui/screens/AlbumCreateScreen.kt
@@ -1,122 +1,154 @@
 package com.uniandes.miso.vinyls.ui.screens
 
-import android.os.Build
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.uniandes.miso.vinyls.R
-import com.uniandes.miso.vinyls.models.Album
 import com.uniandes.miso.vinyls.utils.MainAppBar
-import com.uniandes.miso.vinyls.viewmodels.AlbumViewModel
 
 @Composable
 fun AlbumCreateScreen(
-    albumViewModel: AlbumViewModel = hiltViewModel(),
-    navController: NavHostController
 ) {
-
-    val newAlbum = albumViewModel.album.observeAsState()
-
     Scaffold(
-        topBar = { MainAppBar(navController, R.string.albums) }
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.create_album)) })
+        }
     ) { padding ->
         AlbumCreate(
             modifier = Modifier.padding(padding),
-            newAlbum
         )
     }
 }
 
 @Composable
-fun AlbumCreate(modifier: Modifier, album: State<Album?>) {
+fun AlbumCreate(modifier: Modifier) {
 
     Column{
-        val focusManager = LocalFocusManager.current
+       // val focusManager = LocalFocusManager.current
 
-        album.value?.let { albumItem ->
-            AppTextField(
-                text = albumItem.name,
-                placeholder = "Nombre del Album",
-                /*onChange = {
-                    viewModel.firstName = it
-                },*/
-                imeAction = ImeAction.Next,//Show next as IME button
-                keyboardType = KeyboardType.Text, //Plain text keyboard
-                keyBoardActions = KeyboardActions(
-                    onNext = {
-                        focusManager.moveFocus(FocusDirection.Down)
-                    }
-                )
-            )
-
-            AppTextField(
-                text = albumItem.cover,
-                placeholder = "Url Portada del Album",
-                /*onChange = {
-                    viewModel.firstName = it
-                },*/
-                imeAction = ImeAction.Next,//Show next as IME button
-                keyboardType = KeyboardType.Text, //Plain text keyboard
-                keyBoardActions = KeyboardActions(
-                    onNext = {
-                        focusManager.moveFocus(FocusDirection.Down)
-                    }
-                )
-            )
-
-
+        val name = remember {
+            mutableStateOf(TextFieldValue())
         }
+        val cover = remember {
+            mutableStateOf(TextFieldValue())
+        }
+
+        val releaseDate = remember {
+            mutableStateOf(TextFieldValue())
+        }
+
+        val description = remember {
+            mutableStateOf(TextFieldValue())
+        }
+
+        val genre = remember {
+            mutableStateOf(TextFieldValue())
+        }
+
+        val recordLabel = remember {
+            mutableStateOf(TextFieldValue())
+        }
+
+        Text(
+            text = "Crear Album",
+            style = TextStyle(fontSize = 18.sp, color = Color.Black)
+        )
+
+        Spacer(
+            modifier = Modifier.height(15.dp)
+        )
+
+
+        TextField(
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.LightGray, placeholderColor = Color.Black),
+            label = { Text(text = "Nombre", style = TextStyle(fontSize = 18.sp, color = Color.Black)) },
+            value = name.value,
+            onValueChange = { name.value = it },
+            placeholder = { Text(text = "Nombre") },
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        TextField(
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.LightGray, placeholderColor = Color.Black),
+            label = { Text(text = "Portada", style = TextStyle(fontSize = 18.sp, color = Color.Black)) },
+            value = cover.value,
+            onValueChange = { cover.value = it },
+            placeholder = { Text(text = "Portada") },
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        TextField(
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.LightGray, placeholderColor = Color.Black),
+            label = { Text(text = "Fecha de lanzamiento", style = TextStyle(fontSize = 18.sp, color = Color.Black)) },
+            value = releaseDate.value,
+            onValueChange = { releaseDate.value = it },
+            placeholder = { Text(text = "Fecha de lanzamiento") },
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        TextField(
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.LightGray, placeholderColor = Color.Black),
+            label = { Text(text = "Descripción", style = TextStyle(fontSize = 18.sp, color = Color.Black)) },
+            value = description.value,
+            onValueChange = { description.value = it },
+            placeholder = { Text(text = "Descripción") },
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        TextField(
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.LightGray, placeholderColor = Color.Black),
+            label = { Text(text = "Género", style = TextStyle(fontSize = 18.sp, color = Color.Black)) },
+            value = genre.value,
+            onValueChange = { genre.value = it },
+            placeholder = { Text(text = "Género") },
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        TextField(
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.LightGray, placeholderColor = Color.Black),
+            label = { Text(text = "Sello Discográfico", style = TextStyle(fontSize = 18.sp, color = Color.Black)) },
+            value = recordLabel.value,
+            onValueChange = { recordLabel.value = it },
+            placeholder = { Text(text = "Sello Discográfico") },
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = {},
+                shape = RectangleShape,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Black,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Crear Album")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(15.dp))
     }
 }
 
-@Composable
-fun AppTextField(
-    modifier: Modifier = Modifier,
-    text: String,
-    placeholder: String,
-    leadingIcon: @Composable (() -> Unit)? = null,
-    onChange: (String) -> Unit = {},
-    imeAction: ImeAction = ImeAction.Next,
-    keyboardType: KeyboardType = KeyboardType.Text,
-    keyBoardActions: KeyboardActions = KeyboardActions(),
-    isEnabled: Boolean = true
-) {
-    OutlinedTextField(
-        modifier = modifier.fillMaxWidth(),
-        value = text,
-        onValueChange = onChange,
-        leadingIcon = leadingIcon,
-        textStyle = TextStyle(fontSize = 18.sp),
-        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
-        keyboardActions = keyBoardActions,
-        enabled = isEnabled,
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = Color.Black,
-            unfocusedBorderColor = Color.Gray,
-            disabledBorderColor = Color.Gray,
-            disabledTextColor = Color.Black
-        ),
-        placeholder = {
-            Text(text = placeholder, style = TextStyle(fontSize = 18.sp, color = Color.LightGray))
-        }
-    )
-}
